fd5d42c6cceee488e187b010da2aefb0c0e01abe
CapocaG <agabrielcar@gmail.com>
2023-07-03 13:13:47 -0500

    listo

--- src/main/java/com/example/holacodespace/YoController.java
+++ src/main/java/com/example/holacodespace/YoController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping(path="/")
+@RequestMapping(path="/idat")
 public class YoController {
     @GetMapping(path="/")
     public String completo(){
         return "AT77465395-Arturo Gabriel Carrasco";
     }
-    @GetMapping("/idat/codigo")
+    @GetMapping(path="/codigo")
     public String codigo(){
         return "AT77465395";
     }
-    @GetMapping("/idat/nombre-completo")
+    @GetMapping(path = "/nombre-completo")
     public String nombre(){
         return "Arturo Gabriel Carrasco";
     }
